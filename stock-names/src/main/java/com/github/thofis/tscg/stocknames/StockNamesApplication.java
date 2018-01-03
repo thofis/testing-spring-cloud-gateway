e598318f752bedd813a0b2fdba8c0af31bda8f0c
Thomas Fischer <tf@media-nox.de>
2018-01-03 10:35:22 +0100

    initial commit - project layout

---
+++ stock-names/src/main/java/com/github/thofis/tscg/stocknames/StockNamesApplication.java
@@ -0,0 +1,12 @@
+package com.github.thofis.tscg.stocknames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockNamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockNamesApplication.class, args);
+	}
+}
---
+++ stock-values/src/main/java/com/github/thofis/tscg/stockvalues/StockValuesApplication.java
@@ -0,0 +1,12 @@
+package com.github.thofis.tscg.stockvalues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockValuesApplication.class, args);
+	}
+}
